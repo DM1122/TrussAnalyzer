9c76616404b60825e7511eba2ae8d9d1cf3e571c
DM1122 <D.Maranto1122@gmail.com>
2019-10-18 22:05:18 -0400

    Began preliminary work on visualization and truss data structures

---
+++ truss_analyzer.py
@@ -0,0 +1,193 @@
+# A script designed to analyze trusses (all of them)
+# David Maranto
+
+import turtle
+import veclib
+
+disp_size = [1000,500]
+disp_scl = 25       # every meter represents disp_scl pixels
+disp_speed = 2
+
+
+# joints = [      # [x-coord, y-coord, support?]
+#     [0,0,1],
+#     [5,0,0],
+#     [11,0,1],
+#     [2.5,3,0],
+#     [7.5,3,0],
+# ]
+
+
+joints = {
+    'A':{
+        'pos':[0,0],
+        'support?':True
+    },
+
+    'B':{
+        'pos':[5,0],
+        'support?':True
+    },
+
+    'C':{
+        'pos':[2.5,3],
+        'support?':False
+    }
+}
+
+
+members = {
+    'AB':{
+        'start':joints['A']['pos'],
+        'end':joints['B']['pos'],
+        'dir':None,
+        'length':None,
+        'force':None
+    },
+
+    'AC':{
+        'start':joints['A']['pos'],
+        'end':joints['C']['pos'],
+        'dir':None,
+        'length':None,
+        'force':None
+    },
+
+    'CB':{
+        'start':joints['C']['pos'],
+        'end':joints['B']['pos'],
+        'dir':None,
+        'length':None,
+        'force':None
+    }
+
+}
+
+
+#should be automatically generated and populated
+# angles = {}
+
+
+
+
+def disp_setup():
+    tim.shape('classic')
+    tim.color('black')
+    tim.pensize(3)
+    tim.speed(disp_speed)
+    
+    screen = turtle.Screen()
+    screen.setup(width=disp_size[0], height=disp_size[1], startx=None, starty=None)
+    screen.title('Truss Analysis')
+
+
+def disp_draw_axes():
+    tim.penup()
+    tim.home()
+    
+    axes_length_x = max([joints[i]['pos'][0] for i in joints])
+
+    # axes_length_y = max([joints[i][1] for i in range(len(joints))])
+    axes_length_y = max([joints[i]['pos'][1] for i in joints])
+
+    tim.pendown()
+    tim.backward(1*disp_scl)
+    tim.forward(1*disp_scl)
+    tim.forward(axes_length_x*disp_scl)
+    tim.home()
+
+    tim.setheading(90)
+    tim.backward(1*disp_scl)
+    tim.forward(1*disp_scl)
+    tim.forward(axes_length_y*disp_scl)
+    tim.home()
+
+    tim.penup()
+
+
+def disp_draw_joints():
+    for joint in joints:
+        tim.penup()
+        tim.goto(veclib.scal(joints[joint]['pos'],disp_scl))
+        
+        tim.pendown()
+        if joints[joint]['support?']:
+            tim.setheading(90)
+            tim.backward(10)
+            tim.shape('triangle')
+            tim.stamp()
+            tim.shape('classic')
+            tim.forward(10)
+            tim.setheading(0)
+
+        tim.dot(10,'red')
+
+
+def disp_draw_members():
+    tim.color('blue')
+    for member in members:
+        tim.penup()
+        tim.goto(veclib.scal(members[member]['start'], disp_scl))
+        tim.pendown()
+        tim.goto(veclib.scal(members[member]['end'], disp_scl))
+    
+    tim.color('black')
+
+
+def calc_dirs():
+    for member in members:
+        dir = veclib.sub(members[member]['start'], members[member]['end'])
+        members[member]['dir'] = dir
+
+
+def calc_lengths():
+    for member in members:
+        l = veclib.mag(members[member]['dir'])
+        members[member]['length'] = l
+
+
+def calc_angs():
+    angs = {}
+
+
+    # find all member pairs and add them to pair array (excluding pairs bisected by a member)
+    pairs = []
+    for joint in joints:
+        
+
+        connected_members = []
+        for member in members:
+            if (members[member]['start'] == joints[joint]['pos']) or (members[member]['end'] == joints[joint]['pos']):      # check if a member is connected to this joint
+                connected_members.append(member)
+        
+        print(connected_members)
+
+        ang = veclib.ang(,)
+
+        # angs[joint] = 
+
+    # 
+
+
+
+
+
+if __name__ == '__main__':
+    tim = turtle.Turtle()
+
+    disp_setup()
+    disp_draw_axes()
+    disp_draw_joints()
+    disp_draw_members()
+
+    calc_dirs()
+    calc_lengths()
+    calc_angs()
+
+
+    input("Press Enter to continue...")
+
+
+    #turtle.bye()
+
+
---
+++ veclib.py
@@ -0,0 +1,205 @@
+# David Maranto
+# A library containing useful vector operations
+# Rev 2.0
+
+import math
+
+
+def mag(a):
+    '''
+    Returns magnitiude of vector a.
+
+    Args:
+     a (list): vector
+
+    >>>mag([1,1])
+    1.4142135623730951
+    '''
+
+    sum = 0
+    for e in a:
+        sum += e**2
+
+    mag = math.sqrt(sum)
+
+
+    return mag
+
+
+def sub(a,b):
+    '''
+    Subtracts vectors a b and returns vector c. Assumes vectors are of equal dims.
+
+    Args:
+     a (list): point/vector at tail
+     b (list): point/vector at head
+
+    >>>vecsub([1,2], [3,4])
+    [2,2]
+    '''
+
+    c = [b[i]-a[i] for i in range(len(a))]
+
+
+    return c
+
+def magsub(a,b):
+    '''
+    Combined mag and sub functions: mag(sub())
+    '''
+
+    c = mag(sub(a,b))
+
+    return c
+
+
+def unit(a):
+    '''
+    Returns a unit vector in the same direction as a.
+
+    Args:
+     a (list): a vector
+
+    >>>unit([2,2])
+    [1,1]
+    '''
+
+    a_mag = mag(a)
+    a_unit = [a[i]/a_mag for i in range(len(a))]
+
+
+    return a_unit
+
+
+def dot(a,b):
+    '''
+    Computes the dot product between two vectors. Assumes equal dims.
+
+    Args:
+     a (list): vector
+     b (list): vector
+    
+    >>>dot([1,2], [3,4])
+    11
+    '''
+
+    dot = 0
+    for i in range(len(a)):
+        dot += a[i]*b[i]
+
+    #sum((a*b) for a, b in zip(v1, v2))
+
+    return dot
+
+
+def scal(a,c):
+    '''
+    Performs scalar multiplication on vector a by scalar c.
+
+    Args:
+     a (list): vector
+     c (num): scalar
+    
+    >>>scal([1,2], 3)
+    [3,6]
+    '''
+
+    a = [a[i]*c for i in range(len(a))]
+
+
+    return a
+
+
+def proj(a,b):
+    '''
+    Projects vector a onto b
+
+    Args:
+     a (list): vector to be projected
+     b (list): vector to be projected onto
+    
+    >>>proj([1,1], [1,0])
+    [1,0]
+    '''
+
+    # may consider deriving projection from cosine formula instead
+    scalar = dot(a, unit(b))
+
+    proj = scal(unit(b), scalar)
+
+
+    return proj
+
+
+def cross(a,b):
+    '''
+    Returns the cross of two vectors. Assumes vectors exist in R3. Will pad vectors in < R3 automatically.
+
+    Args:
+     a (list): vector
+     b (list): vector
+    
+    >>>cross([1,0,0], [0,1,0])
+    [0,0,1]
+    '''
+    
+    # padding
+    while len(a) != 3:
+        a.append(0)
+
+    while len(b) != 3:
+        b.append(0)
+
+
+    c = []
+    c.append(a[1]*b[2] - a[2]*b[1])
+    c.append(a[2]*b[0] - a[0]*b[2])
+    c.append(a[0]*b[1] - a[1]*b[0])
+
+
+    return c
+
+
+def ang(a,b):
+    '''
+    Computes the angle between vectors a and b. Returns angle in degrees.
+
+    Args:
+     a (list): vector
+     b (list): vector
+    
+    >>>ang([1,1], [1,0])
+    45
+    '''
+
+    # Not working for ang > 90
+    # adj = mag(proj(a,b))
+    # hyp = mag(a)
+
+    # ang = math.acos(adj / hyp)
+
+    ang = math.acos( dot(a,b) / (mag(a)*mag(b)) )
+
+
+    return math.degrees(ang)
+
+
+
+if __name__ == '__main__':
+    a = [1,2]
+    b = [3,4]
+    c = 2
+
+    print("Testing veclib...")
+    print('a:', a)
+    print('b:', b)
+
+    print('ang():', ang(a,b))
+    print('cross():', cross(a,b))
+    print('dot():', dot(a,b))
+    print('mag():', mag(a))
+    print('proj():', proj(a,b))
+    print('scal():', scal(a,c))
+    print('sub():', sub(a,b))
+    print('unit():', unit(a))
+
