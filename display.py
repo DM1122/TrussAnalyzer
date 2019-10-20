2ede3c012644bcf1aa2dddce2a615706faa2d6e6
DM1122 <D.Maranto1122@gmail.com>
2019-10-20 17:41:31 -0400

    Began migrating visualizer methods to standalone display.py

---
+++ display.py
@@ -0,0 +1,78 @@
+# truss visualizer using turtle graphics
+
+tim = turtle.Turtle()
+
+def setup():
+    tim.shape('classic')
+    tim.color('black')
+    tim.pensize(3)
+    tim.speed(disp_speed)
+    
+    screen = turtle.Screen()
+    screen.setup(width=disp_size[0], height=disp_size[1], startx=None, starty=None)
+    screen.title('Truss Analysis')
+
+
+def draw_axes():
+    tim.penup()
+    tim.home()
+    
+    axes_length_x = max([joints[i]['pos'][0] for i in joints])
+    axes_length_y = max([joints[i]['pos'][1] for i in joints])
+
+    tim.pendown()
+    tim.backward(1*disp_scl)
+    tim.forward(1*disp_scl)
+    tim.forward(axes_length_x*disp_scl)
+    tim.home()
+
+    tim.setheading(90)
+    tim.backward(1*disp_scl)
+    tim.forward(1*disp_scl)
+    tim.forward(axes_length_y*disp_scl)
+    tim.home()
+
+    tim.penup()
+
+
+def draw_joints():
+    for joint in joints:
+        tim.penup()
+        tim.goto(veclib.scal(joints[joint]['pos'],disp_scl))
+        
+        tim.pendown()
+        if joints[joint]['support'] is not None:
+            tim.setheading(90)
+            tim.backward(10)
+            tim.shape('triangle')
+            tim.stamp()
+            tim.shape('classic')
+            tim.forward(10)
+            tim.setheading(0)
+
+        tim.dot(10,'red')
+
+
+def draw_members():
+    tim.color('blue')
+    for member in members:
+        tim.penup()
+        tim.goto(veclib.scal(members[member]['start'], disp_scl))
+        tim.pendown()
+        tim.goto(veclib.scal(members[member]['end'], disp_scl))
+    
+    tim.color('black')
+
+
+def draw_forces():
+    for f in forces:
+        tim.penup()
+        tim.goto(veclib.scal(forces[f]['pos'],disp_scl))
+        tim.setheading(forces[f]['ang'])
+        tim.backward(10)
+        tim.color('green')
+        tim.shape('arrow')
+        tim.stamp()
+        tim.shape('classic')
+        tim.color('black')
+
--- truss_analyzer.py
+++ truss_analyzer.py
@@ -4,7 +4,9 @@
 import math
 import turtle
 
+import display as disp
 import veclib
+
 
 disp_size = [1000,500]
 disp_scl = 25       # every meter represents disp_scl pixels
@@ -30,7 +32,6 @@ joints = {
         'support':None,
     }
 }
-
 
 members = {
     'AB':{
@@ -66,84 +67,6 @@ forces = {
 
 
 
-
-def disp_setup():
-    tim.shape('classic')
-    tim.color('black')
-    tim.pensize(3)
-    tim.speed(disp_speed)
-    
-    screen = turtle.Screen()
-    screen.setup(width=disp_size[0], height=disp_size[1], startx=None, starty=None)
-    screen.title('Truss Analysis')
-
-
-def disp_draw_axes():
-    tim.penup()
-    tim.home()
-    
-    axes_length_x = max([joints[i]['pos'][0] for i in joints])
-
-    # axes_length_y = max([joints[i][1] for i in range(len(joints))])
-    axes_length_y = max([joints[i]['pos'][1] for i in joints])
-
-    tim.pendown()
-    tim.backward(1*disp_scl)
-    tim.forward(1*disp_scl)
-    tim.forward(axes_length_x*disp_scl)
-    tim.home()
-
-    tim.setheading(90)
-    tim.backward(1*disp_scl)
-    tim.forward(1*disp_scl)
-    tim.forward(axes_length_y*disp_scl)
-    tim.home()
-
-    tim.penup()
-
-
-def disp_draw_joints():
-    for joint in joints:
-        tim.penup()
-        tim.goto(veclib.scal(joints[joint]['pos'],disp_scl))
-        
-        tim.pendown()
-        if joints[joint]['support'] is not None:
-            tim.setheading(90)
-            tim.backward(10)
-            tim.shape('triangle')
-            tim.stamp()
-            tim.shape('classic')
-            tim.forward(10)
-            tim.setheading(0)
-
-        tim.dot(10,'red')
-
-
-def disp_draw_members():
-    tim.color('blue')
-    for member in members:
-        tim.penup()
-        tim.goto(veclib.scal(members[member]['start'], disp_scl))
-        tim.pendown()
-        tim.goto(veclib.scal(members[member]['end'], disp_scl))
-    
-    tim.color('black')
-
-
-def disp_draw_forces():
-    for f in forces:
-        tim.penup()
-        tim.goto(veclib.scal(forces[f]['pos'],disp_scl))
-        tim.setheading(forces[f]['ang'])
-        tim.backward(10)
-        tim.color('green')
-        tim.shape('arrow')
-        tim.stamp()
-        tim.shape('classic')
-        tim.color('black')
-
-
 def calc_dirs():
     for member in members:
         dir = veclib.sub(members[member]['start'], members[member]['end'])
@@ -176,7 +99,7 @@ def det_joint_members():
         joints[joint]['connected'] = connected
 
 
-# def calc_support_rxns():
+
     
 def calc_net_moment(origin):
     sum = 0
@@ -193,7 +116,7 @@ def calc_net_moment(origin):
 
 
 if __name__ == '__main__':
-    tim = turtle.Turtle()
+    
 
     disp_setup()
     disp_draw_axes()
@@ -226,7 +149,7 @@ if __name__ == '__main__':
 
     print(joints[joint_roller])
 
-    joints[]
+    joints[joint_pin]['reaction'] = []
 
     input("Press Enter to continue...")
 
